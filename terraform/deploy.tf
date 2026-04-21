551b44276c5cc09f3fed08f45cd60263e46fc52c
isanivska123 <154686793+isanivska123@users.noreply.github.com>
2026-04-21 21:11:49 +0300

    Rollback to us-east-1 region and stable Ubuntu AMI

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  region = "eu-north-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "coordinator" {
-  ami           = "ami-0705383d0b3ee1b10"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0c558256fc8403fb0"]
-  tags = { Name = "Coordinator" }
+  vpc_security_group_ids = [aws_security_group.laba_sg.id]
+  tags = { Name = "Sharding-Coordinator" }
 }
 
 resource "aws_instance" "shards" {
   count         = 2
-  ami           = "ami-0705383d0b3ee1b10"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-0c558256fc8403fb0"]
-  tags = { Name = "Shard-${count.index}" }
+  vpc_security_group_ids = [aws_security_group.laba_sg.id]
+  tags = { Name = "Shard-Node-${count.index + 1}" }
 }
